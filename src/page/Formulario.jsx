61d936f572bcc1711915ae47fbb76ceae79ae58e
GabrielBarcell0s <gabrielpbarcellos@gmail.com>
2022-09-21 17:52:44 -0300

    Formik Configurado usando RegEx

--- src/page/Formulario.jsx
+++ src/page/Formulario.jsx
@@ -3,14 +3,23 @@ import { Form, Field, Formik } from 'formik'
 import * as Yup from 'yup'
 
 function Formulario() {
+  const RegExp = {
+    nome: /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/,
+    telefone: /^\+?\d{2}?\s*\(\d{2}\)?\s*\d{4,5}\-?\d{4}$/g
+  }
+
   const schema = Yup.object().shape({
-    PrimeiroNome: Yup.string().required(),
-    Sobrenome: Yup.string().required(),
-    Email: Yup.string().email(),
-    Telefone: Yup.string().length(11),
-    Idade: Yup.number().positive().integer().min(15)
+    PrimeiroNome: Yup.string().matches(RegExp.nome).required(),
+    Sobrenome: Yup.string().matches(RegExp.mome).required(),
+    Email: Yup.string().email().required(),
+    Telefone: Yup.string().trim().matches(RegExp.telefone).required(),
+    DataNasc: Yup.date().required()
   })
 
+  const onSubmit = (values, actions) => {
+
+  }
+  
 
   return (
     <>
@@ -18,51 +27,54 @@ function Formulario() {
 
       <Formik
         validationSchema={schema}
+        onSubmit={onSubmit}
 
         initialValues={{
           PrimeiroNome: "",
           Sobrenome: "",
-          Email:"",
-          Telefone:"",
-          DataNasc:""
+          Email: "",
+          Telefone: "",
+          DataNasc: ""
         }}
       >
-        {({ errors }) => (
+        {({ errors, isValid }) => (
           <Form>
             <div>
               <label htmlFor="PrimeiroNome">Nome:</label>
-              <Field id="PrimeiroNome" name="PrimeiroNome" type="text"></Field>
+              <Field id="PrimeiroNome" name="PrimeiroNome" type="text" placeholder="João"></Field>
             </div>
 
             {errors.PrimeiroNome}
 
             <div>
               <label htmlFor="Sobrenome">Sobrenome:</label>
-              <Field id="Sobrenome" name="Sobrenome" type="text"></Field>
+              <Field id="Sobrenome" name="Sobrenome" type="text" placeholder="Santos"></Field>
             </div>
 
             {errors.Sobrenome}
 
             <div>
               <label htmlFor="Email">Email:</label>
-              <Field id="Email" name="Email" type="email" />
+              <Field id="Email" name="Email" type="email" placeholder="joaoExemple@exemple.com"/>
             </div>
 
             {errors.Email}
 
             <div>
               <label htmlFor="Telefone">Telefone</label>
-              <Field id="Telefone" name="Telefone" type="tel" />
+              <Field id="Telefone" name="Telefone" type="tel" placeholder="+99(99)9 9999-9999" />
             </div>
 
             {errors.Telefone}
 
             <div>
-              <label htmlFor="Idade">Idade:</label>
-              <Field id="Idade" name="Idade" type="number" />
+              <label htmlFor="DataNasc">Data de Nascimento:</label>
+              <Field id="DataNas" name="DataNasc" type="date"/>
             </div>
 
-            {errors.Idade && "????"}
+            {errors.DataNasc}
+
+            <button disabled={!isValid} type="submit">Submit</button>
 
           </Form>
         )}
